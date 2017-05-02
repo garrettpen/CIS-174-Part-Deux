8421e2d149ce4b0a0d7935647ffd2f11958fc05d
Kirby Simmons <kirbyjamessimmons@gmail.com>
2017-05-02 17:37:34 -0500

    Cleaned some code

--- budgetTracker 2/budgetTracker/Default.aspx.designer.cs
+++ budgetTracker 2/budgetTracker/Default.aspx.designer.cs
@@ -157,15 +157,6 @@ namespace budgetTracker {
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
         
         /// <summary>
-        /// tableExpenses control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlTable tableExpenses;
-        
-        /// <summary>
         /// expenseGrid control.
         /// </summary>
         /// <remarks>
